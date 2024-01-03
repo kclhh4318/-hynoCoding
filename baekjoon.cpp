d7f4e6f1178b71101c6c2f417f8568fbfc22b7a3
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-03 23:01:15 +0900

    백준 5622

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main(){
 
-    cout << "hello world!" << endl;
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    string word;
+    int time = 0;
+    cin >> word;
+
+    for(int i = 0; i < word.length(); i++){
+        if(word[i] <= 'C') time += 3;
+        else if(word[i] <= 'F') time += 4;
+        else if(word[i] <= 'I') time += 5;
+        else if(word[i] <= 'L') time += 6;
+        else if(word[i] <= 'O') time += 7;
+        else if(word[i] <= 'S') time += 8;
+        else if(word[i] <= 'V') time += 9;
+        else if(word[i] <= 'Z') time += 10;
+        
+    }
+
+    cout << time << endl;
 
     return 0;
 }
