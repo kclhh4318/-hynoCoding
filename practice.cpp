084e8a3d9b39e56ab65df1c9d5dfd2a9b8baf2d1
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-03 23:30:47 +0900

    이건 메인 브랜치에서 왜 커밋되지

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string str = "Hello";
+
+    if(int(str[0]) == 72) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    return 0;
+}
