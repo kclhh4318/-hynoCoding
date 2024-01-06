a9374adb6b1993ebce60d51d5774fcc9ea678e5a
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-06 11:08:25 +0900

    2444 다시 풀어보기

--- prac.cpp
+++ prac.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
+
 int main(){
-    int N = 0;
+
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    int N;
     cin >> N;
+
     for(int i = 1; i <= N; i++){
-        for(int j = N - i; j > 0; j--){
-            cout << " ";
-        }
-        for(int j = 2*i-1; j > 0; j--){
-            cout << "*";
-        }
+        for(int j = N - i; j > 0; j--) cout << ' ';
+        for(int j = 2 * i - 1; j > 0; j--) cout << '*';
         cout << endl;
     }
-    
+
     for(int i = 1; i < N; i++){
-        for (int j = 0; j < i; j++){
-            cout << " ";
-        }
-        for(int j = 2*(N-i) -1; j > 0; j--){
-            cout << "*";
-        }
+        for(int j = i; j > 0; j--) cout << ' ';
+        for(int j = 2 * (N - i) - 1; j > 0; j--) cout << '*';
         cout << endl;
     }
+
     return 0;
 }
