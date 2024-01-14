395b93f775ad9895223ad2f72340d23415a0bfec
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-14 13:06:01 +0900

    2023.01.14

--- BOJ25206.cpp
+++ BOJ25206.cpp
@@ -5,56 +5,28 @@ int main(){
 
     string subject;
     string rate;
-    double hak, avg_rate = 0.0;
-    int pf_cnt = 0;
+    double hak, hakSum = 0.0, avgSum = 0.0;
 
     for(int i = 0; i < 20; i++){
-        int temp;
+        double temp;
         cin >> subject >> hak >> rate;
 
         if(rate == "P"){
-            pf_cnt++;
             continue;
         }
-        else if(rate == "A+"){
-            temp = 4.5 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "A0"){
-            temp = 4.0 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "B+"){
-            temp = 3.5 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "B0"){
-            temp = 3.0 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "C+"){
-            temp = 2.5 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "C0"){
-            temp = 2.0 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "D+"){
-            temp = 1.5 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "D0"){
-            temp = 1.5 * hak;
-            avg_rate += temp;
-        }
-        else if(rate == "F"){
-            avg_rate += 0;
-        }
+        else if(rate == "A+") avgSum += (4.5 * hak);
+        else if(rate == "A0") avgSum += (4.0 * hak);
+        else if(rate == "B+") avgSum += (3.5 * hak);
+        else if(rate == "B0") avgSum += (3.0 * hak);
+        else if(rate == "C+") avgSum += (2.5 * hak);
+        else if(rate == "C0") avgSum += (2.0 * hak);
+        else if(rate == "D+") avgSum += (1.5 * hak);
+        else if(rate == "D0") avgSum += (1.0 * hak);
+        else if(rate == "F") avgSum += (0.0 * hak);
+        hakSum += hak;
     }
 
-    cout.precision(6);
-    cout << avg_rate / (double)(20 - pf_cnt) << endl;
+    cout << (double)avgSum / (double)hakSum << endl;
 
 
     return 0;
