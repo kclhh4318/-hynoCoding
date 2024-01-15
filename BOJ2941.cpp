111d07f01cc44f29d90ad460c88fd5ac22d38695
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-16 00:39:31 +0900

    시간복잡도가 줄어든 문제

--- BOJ2941.cpp
+++ BOJ2941.cpp
@@ -1,26 +1,26 @@
 //크로아티아 알파벳
 
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
-int main(){
-
-    vector<string> croatian = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+int main() {
     string str;
     cin >> str;
 
-    for(int i = 0; i < croatian.size(); i++){
-        while(1){
-            int idx;
-            idx = str.find(croatian[i]);
-            if(idx == string::npos) break;
-            str.replace(idx, croatian[i].length(), "$");
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'c' && (str[i+1] == '=' || str[i+1] == '-')) {
+            i++;
+        } else if (str[i] == 'd' && str[i+1] == 'z' && str[i+2] == '=') {
+            i += 2;
+        } else if ((str[i] == 'd' && str[i+1] == '-') || (str[i] == 'l' && str[i+1] == 'j') || (str[i] == 'n' && str[i+1] == 'j') || (str[i] == 's' && str[i+1] == '=') || (str[i] == 'z' && str[i+1] == '=')) {
+            i++;
         }
+        count++;
     }
 
-    cout << str.length() << endl;
+    cout << count << endl;
 
     return 0;
 }
