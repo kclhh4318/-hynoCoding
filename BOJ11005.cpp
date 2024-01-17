d46b74531096b6f25698e45f18e60730dc08108b
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-17 16:44:38 +0900

    2023.01.17

---
+++ BOJ11005.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, B;
+    string result = "";
+
+    cin >> N >> B;
+
+    for(int i = N; i > 0; i /= B){
+        if(i % B >= 10) result += (char)(i % B - 10 + 'A');
+        else result += (char)(i % B + '0');
+    }
+
+    reverse(result.begin(), result.end());
+    
+    cout << result << '\n';
+
+    return 0;
+}
