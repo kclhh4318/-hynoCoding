46c59496a1dabab2f5836455f21d390677406e2f
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-17 15:29:10 +0900

    2023.01.17

---
+++ BOJ2563.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int const drawingPaperSIZE = 100;
+int arr[drawingPaperSIZE][drawingPaperSIZE] = {0,};
+int const colorSIZE = 10;
+
+int main(){
+
+    int N, A = 0;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        int x, y;
+        cin >> x >> y;
+        for(int j = x; j < x + colorSIZE; j++){
+            for(int k = y; k < y + colorSIZE; k++){
+                arr[j][k] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < drawingPaperSIZE; i++){
+        for(int j = 0; j < drawingPaperSIZE; j++){
+            if(arr[i][j] == 1) A++;
+         }
+    }
+
+    cout << A << '\n';
+
+    return 0;
+}
