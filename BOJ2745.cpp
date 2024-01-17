cba5d50bf5bb244051f55554adae997f1f690964
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-17 16:10:58 +0900

    2023.01.17

--- BOJ2745.cpp
+++ BOJ2745.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main(){
 
-    cin.tie(0);
+    cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
     string N;
+
     int B;
 
     cin >> N >> B;
@@ -15,12 +16,8 @@ int main(){
     int temp = 1;
 
     for(int i = N.length() - 1; i >= 0; i--){
-        if(N[i] >= '0' && N[i] <= '9'){
-            result += (N[i] - '0') * temp;
-        }
-        else{
-            result += (N[i] - 'A' + 10) * temp;
-        }
+        if(N[i] >= '0' && N[i] <= '9') result += (N[i] - '0') * temp;
+        else result += (N[i] - 'A' + 10) * temp;
         temp *= B;
     }
 
