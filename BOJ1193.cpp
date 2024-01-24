5d4b4c1c1e9c4c898d704e801ff9c3dcad538c50
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-24 23:06:26 +0900

    2023.01.24

--- BOJ1193.cpp
+++ BOJ1193.cpp
@@ -12,7 +12,14 @@ int main(){
 
     cin >> X;
 
-    
+    int i = 1;
+    while(X > i){
+        X -= i;
+        i++;
+    }
+
+    if(i % 2 == 0) cout << X << '/' << i - X + 1 << '\n';
+    else cout << i - X + 1 << '/' << X << '\n';
 
     return 0;
 }
