57753eef8d3e9c3666ff6c39c062fc2d39880b34
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-27 15:33:23 +0900

    2023.01.27

---
+++ BOJ15894.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    long long n;
+    cin >> n;
+    cout << 4 * n << '\n';
+
+    return 0;
+}
