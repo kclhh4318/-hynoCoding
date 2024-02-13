9919a6f3988676ae9fca5e73e390c89f5555e7c9
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-02-13 16:45:54 +0900

    2024.02.13

--- practice.cpp
+++ practice.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
-#include <cctype>
-#include <string>
-#include <algorithm>
-#include <map>
+#include <vector>
+#include <unordered_set>
 using namespace std;
 
-string pokemon[100000];
+void def(){
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false);
+}
 
-int main() {
-	map<string, int> P;  
-	int N;               // 도감에 수록된 포켓몬 수
-	int M;               // 맞춰야 하는 문제의 수
-	string input_name;   // 맞춰야 되는 문제 중 입력된 이름
-	int input_num;       // 맞춰야 되는 문제 중 입력된 번호
+int main(){
 
-	cin.tie(0);
-	ios::sync_with_stdio(0);
+	def();
 
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		cin >> pokemon[i];
-		P.insert(make_pair(pokemon[i], i));
-	}
+	int n;
+	string namae, status;
+	cin >> n;
+	unordered_set<string> name;
 
-	for (int i = 0; i < M; i++) {
-		cin >> input_name;
-
-		// 1. 입력된 것이 숫자인 경우
-		if (isdigit(input_name[0]) != 0) {
-			input_num = stoi(input_name) - 1;
-			cout << pokemon[input_num] << "\n";
+	for(int i = 0; i < n; i++){
+		cin >> namae >> status;
+		if(status == "enter"){
+			name.insert(namae);
 		}
-
-		// 2. 입력된 것이 문자인 경우
-		else {
-			auto index = P.find(input_name);
-			cout << index->second + 1 << "\n";
+		else{
+			name.erase(namae);
 		}
 	}
+
+	vector<string> permut(name.begin(), name.end());
+
+	sort(permut.rbegin(), permut.rend());
+
+	for(const auto& it : permut) cout << it << '\n';
+
+	cout << endl;
 
 	return 0;
 }
