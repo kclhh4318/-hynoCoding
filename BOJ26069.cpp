f8d3db54520934065a9beb4a0574fac031288b41
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-02-27 14:07:55 +0900

    2024.02.27

--- BOJ26069.cpp
+++ BOJ26069.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
 void init(){
@@ -11,10 +12,30 @@ int main(){
     init();
 
     int N;
-    vector<string> dance_mamber;
+    string mem1, mem2;
+    set<string> member;
 
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        cin >> mem1 >> mem2;
+        if(mem1 == "ChongChong"){
+            member.insert(mem1);
+            member.insert(mem2);
+        }
+        else if(mem2 == "ChongChong"){
+            member.insert(mem1);
+            member.insert(mem2);
+        }
+        else if(member.find(mem1) != member.end()){
+            member.insert(mem2);
+        }
+        else if(member.find(mem2)  != member.end()){
+            member.insert(mem1);
+        }
+    }
+
+    cout << member.size() << '\n';
     
-
-
     return 0;
 }
